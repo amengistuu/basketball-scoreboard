c773d9e513b7340f398d0dd12879e2cc40c8a574
Abraham <abraham.a.mengistu@gmail.com>
2023-12-26 19:50:11 -0500

    Update code

---
+++ index.js
@@ -0,0 +1,12 @@
+// when a button is clicked, add it to the points
+
+let homePoints = document.getElementById("home-points");
+let guestPoints = document.getElementById("guest-points");
+console.log("Hello");
+
+plusOne = (team) => {
+    console.log("clicked");
+    let counter = document.getElementById(team + "-plus-one")
+    console.log(counter)
+    
+}
